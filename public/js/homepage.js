f84d81ae1975d7d988149b2ab8661d2301fb67ce
Michael Francisco <m.francisco9@gmail.com>
2021-05-11 14:22:59 -0500

    save buttons working for console log

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,33 +1,28 @@
 // setting, editing and deleting top artists
 const setTopArtist = async (event) => {
-    const artists = [];
-    console.log(artists);
-    console.log("artist save")
+    console.log("artists save");
 };
 const editTopArtist = async (event) => {};
 const delTopArtist = async (event) => {};
 // setting, editing, and delting top genres
 const setTopGenre = async (event) => {
-    const genres = [];
-    console.log("genre save")
+console.log("genres save");
+
 };
 const editTopGenre = async (event) => {};
 const delTopGenre = async (event) => {};
 // setting, editing, and deleting top tracks
 const setTopTrack = async (event) => {
-    const tracks = [];
-    console.log("track save")
+   console.log("tracks save");
 };
 const editTopTrack = async (event) => {};
 const delTopTrack = async (event) => {};
 
-
-
 // artist event listeners 
-document.querySelector('#artistsSaveBtn').addEventListener("click", setTopArtist);
+document.querySelector("#artistsSaveBtn").addEventListener("click", setTopArtist);
 
 // genre event listeners
-document.querySelector('#genresSaveBtn').addEventListener("click", setTopGenre);
+document.querySelector("#genresSaveBtn").addEventListener("click", setTopGenre);
 
 // track event listeners
-document.querySelector('#tracksSaveBtn').addEventListener("click", setTopTrack);
+document.querySelector("#tracksSaveBtn").addEventListener("click", setTopTrack);
