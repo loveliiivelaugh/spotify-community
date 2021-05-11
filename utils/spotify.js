123ab41d48bcb42e829c7fa6c992f1835df87582
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-11 12:20:20 -0500

    Updated login with Spotify button. Is working now.

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,14 +1,11 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
 
-const clientId = "5fad7261e6a643669234205c08cc5547";
-const clientSecret = "234bb2ad45224f92b11d7c3d4500acf3";
-const redirectUri = 'http://localhost:8080/callback/';
 
-// credentials are optional
 const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret,
-  redirectUri: redirectUri
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'http://localhost:8080/callback/'
 });
 
 module.exports = spotifyApi;
