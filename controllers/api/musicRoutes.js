e292342a4e199ed54bd15269dc54fbb945ada4a3
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-11 15:01:16 -0500

    Added skeleton for Music routes.

---
+++ controllers/api/musicRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const Music = require('../../models/Music.js')
+
+
+router.get('/artist/:id', (req, res) => {})
+
+router.get('/artists', (req, res) => {})
+
+router.post('/artist', (req, res) => {})
+
+router.put('/artist', (req, res) => {})
+
+router.delete('/artist', (req, res) => {})
+
+
+router.get('/track/:id', (req, res) => {})
+
+router.get('/tracks', (req, res) => {})
+
+router.post('/track', (req, res) => {})
+
+router.put('/track', (req, res) => {})
+
+router.delete('/track', (req, res) => {})
+
+
+router.get('/genre/:id', (req, res) => {})
+
+router.get('/genres', (req, res) => {})
+
+router.post('/genre', (req, res) => {})
+
+router.put('/genre', (req, res) => {})
+
+router.delete('/genre', (req, res) => {})
