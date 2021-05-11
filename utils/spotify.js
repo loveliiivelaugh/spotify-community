870cbfb9e183d66f492c8e4f37d30ddefe2737ec
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-11 12:02:00 -0500

    Updated navigation links and page routes.

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,8 +1,8 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI || 'http://localhost:8080/callback/';
+const clientId = "5fad7261e6a643669234205c08cc5547";
+const clientSecret = "234bb2ad45224f92b11d7c3d4500acf3";
+const redirectUri = 'http://localhost:8080/callback/';
 
 // credentials are optional
 const spotifyApi = new SpotifyWebApi({
