1e27a260187dab393f9354b45950d3996dc1d6c0
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-12 09:30:11 -0500

    Added all musicRoutes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,33 +1,33 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
+// const userRoutes = require('./userRoutes');
 
 const spotifyRoutes = require('./spotifyRoutes');
-const albumRoutes = require('./albumRoutes');
-const artistRoutes = require('./artistRoutes');
-const browseRoutes = require('./browseRoutes');
-const followRoutes = require('./followRoutes');
-const libraryRoutes = require('./libraryRoutes');
-const personalizationRoutes = require('./personalizationRoutes');
-const playlistRoutes = require('./playlistRoutes');
-const searchRoutes = require('./searchRoutes');
-const tracksRoutes = require('./tracksRoutes');
-const userProfileRoutes = require('./userProfileRoutes');
-const playerRoutes = require('./playerRoutes');
+// const albumRoutes = require('./albumRoutes');
+// const artistRoutes = require('./artistRoutes');
+// const browseRoutes = require('./browseRoutes');
+// const followRoutes = require('./followRoutes');
+// const libraryRoutes = require('./libraryRoutes');
+// const personalizationRoutes = require('./personalizationRoutes');
+// const playlistRoutes = require('./playlistRoutes');
+// const searchRoutes = require('./searchRoutes');
+// const tracksRoutes = require('./tracksRoutes');
+// const userProfileRoutes = require('./userProfileRoutes');
+// const playerRoutes = require('./playerRoutes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
 router.use('/spotify', spotifyRoutes);
-router.use('/albums', albumRoutes);
-router.use('/artists', artistRoutes);
-router.use('/browse', browseRoutes);
-router.use('/follow', followRoutes);
-router.use('/library', libraryRoutes);
-router.use('/personalization', personalizationRoutes);
-router.use('/playlist', playlistRoutes);
-router.use('/search', searchRoutes);
-router.use('/tracks', tracksRoutes);
-router.use('/userProfile', userProfileRoutes);
-router.use('/player', playerRoutes);
+// router.use('/albums', albumRoutes);
+// router.use('/artists', artistRoutes);
+// router.use('/browse', browseRoutes);
+// router.use('/follow', followRoutes);
+// router.use('/library', libraryRoutes);
+// router.use('/personalization', personalizationRoutes);
+// router.use('/playlist', playlistRoutes);
+// router.use('/search', searchRoutes);
+// router.use('/tracks', tracksRoutes);
+// router.use('/userProfile', userProfileRoutes);
+// router.use('/player', playerRoutes);
 
 module.exports = router;
--- controllers/api/musicRoutes.js
+++ controllers/api/musicRoutes.js
@@ -2,34 +2,169 @@ const router = require('express').Router();
 const Music = require('../../models/Music.js')
 
 
-router.get('/artist/:id', (req, res) => {})
+router.get('/artist/:id', (req, res) => {
+  try {
+    const artistData = await Music.findByPk(req.params.id);
+    res.status(200).json(artistData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.get('/artists', (req, res) => {})
+router.get('/artists', (req, res) => {
+  try {
+    const artistData = await Music.findAll();
+    res.status(200).json(artistData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.post('/artist', (req, res) => {})
+router.post('/artist', (req, res) => {
+  try {
+    const artistData = await Music.create({
+      artist: req.body.artist
+    });
+    res.status(200).json(artistData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.put('/artist', (req, res) => {})
+router.put('/artist/:id', (req, res) => {})
 
-router.delete('/artist', (req, res) => {})
+router.delete('/artist/:id', (req, res) => {
+  try {
+    const artistData = await Music.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!artistData) {
+      res.status(404).json({ message: 'No artist found with this id!' });
+      return;
+    }
+
+    res.status(200).json(artistData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
 
-router.get('/track/:id', (req, res) => {})
+router.get('/track/:id', (req, res) => {
+  try {
+    const trackData = await Music.findByPk(req.params.id);
+    res.status(200).json(trackData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.get('/tracks', (req, res) => {})
+router.get('/tracks', (req, res) => {
+  try {
+    const trackData = await Music.findAll();
+    res.status(200).json(trackData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.post('/track', (req, res) => {})
+router.post('/track', (req, res) => {
+  try {
+    const trackData = await Music.create({
+      track: req.body.track
+    });
+    res.status(200).json(trackData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.put('/track', (req, res) => {})
+router.put('/track/:id', (req, res) => {})
 
-router.delete('/track', (req, res) => {})
+router.delete('/track/:id', (req, res) => {
+  try {
+    const trackData = await Music.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!trackData) {
+      res.status(404).json({ message: 'No track found with this id!' });
+      return;
+    }
+
+    res.status(200).json(trackData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
 
-router.get('/genre/:id', (req, res) => {})
+router.get('/genre/:id', (req, res) => {
+  try {
+    const genreData = await Music.findByPk(req.params.id);
+    res.status(200).json(genreData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.get('/genres', (req, res) => {})
+router.get('/genres', (req, res) => {
+  try {
+    const genreData = await Music.findAll();
+    res.status(200).json(genreData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.post('/genre', (req, res) => {})
+router.post('/genre', (req, res) => {
+  try {
+    const genreData = await Music.create({
+      genre: req.body.genre
+    });
+    res.status(200).json(genreData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-router.put('/genre', (req, res) => {})
+router.put('/genre/:id', (req, res) => {})
 
-router.delete('/genre', (req, res) => {})
+router.delete('/genre/:id', (req, res) => {
+  try {
+    const genreData = await Music.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!genreData) {
+      res.status(404).json({ message: 'No genre found with this id!' });
+      return;
+    }
+
+    res.status(200).json(genreData)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
