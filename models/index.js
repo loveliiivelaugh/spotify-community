5e744173c9dba9dd1519c79172e1b764292a4ee4
jjayjack <jocelynjayjack@gmail.com>
2021-05-12 10:07:43 -0500

    Updated Models

--- models/index.js
+++ models/index.js
@@ -1,14 +1,22 @@
 //dependencies
 const Song = require('./song');
 const Playlist = require('./playlist');
+const Music = require('./music');
+const User = require('./user');
 
 //Relationships
+
+//A playlist contains many songs
+Playlist.hasMany(Song, {
+
+})
+
 //One song belongs to many playlists
 Song.belongsTo(Playlist, {
     foreignKey: 'playlist_id'
 })
 
-//Many playlists contain many songs
-Playlist.hasMany(Song, {
-
+//Music contains many playlists
+Music.belongsToMany(Playlist, {
+    foreignKey: ''
 })
