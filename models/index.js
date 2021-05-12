297807c6f5eb35400551c893662b7c6f2484b316
jjayjack <jocelynjayjack@gmail.com>
2021-05-12 12:52:18 -0500

    Model Relationships Updated

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@
 const Playlist = require('./playlist');
 const User = require('./user');
 const Song = require('./song');
-const Music = require('./music');
+// const Music = require('./music');
 
 //Relationships
 
-//Playlists belong to one user
-Playlist.belongsTo(User, {
+//Many Playlists belong to one user
+Playlist.belongsToMany(User, {
     foreignKey: 'user_id',
 }) 
 
@@ -17,22 +17,14 @@ User.hasMany(Playlist, {
 });
 
 //A playlist has many songs (One Playlist has many Songs)
-Playlist.hasMany(Song)
+Playlist.hasMany(Song);
 
 //One song belongs to many playlists
-Song.belongsTo(Playlist)
+Song.belongsToMany(Playlist);
 
-// //Music has many playlists
-
-
-
-// Music.belongsToMany(Playlist, {
-//     foreignKey: ''
-// }) 
 
 module.exports = {
     Song,
     Playlist,
-    Music,
     User,    
 }
