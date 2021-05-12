2db3f09c57ac4b53eeab3e33e9a3a8e0ee2a4273
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-12 12:47:16 -0500

    Added an example route to the homepage routes to interact with fav pref.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,6 +1,21 @@
 // setting, editing and deleting top artists
 const setTopArtist = async (event) => {
-    console.log("artists save");
+    console.log("artists save", event.target);
+    try {
+        const example = {
+            artist: 'red hot chili peppers'
+        }
+
+        const response = await fetch('/api/music/artists', {
+            method: "POST",
+            body: JSON.stringify(example),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })   
+    } catch (error) {
+        console.error(error);
+    }
 };
 const editTopArtist = async (event) => {};
 const delTopArtist = async (event) => {};
