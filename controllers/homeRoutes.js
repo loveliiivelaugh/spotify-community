f50045e0e525417a50025bae74aacadf9dc9e757
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-12 12:23:37 -0500

    Finished all Authentication.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('homepage', {
       ...user,
       logged_in: req.session.logged_in
     });
@@ -47,7 +47,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/playlists');
     return;
   }
 
